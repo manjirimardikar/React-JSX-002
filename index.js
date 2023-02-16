84b259b3e6301d10ae653406fe3cef930899cd2c
manjirimardikar <manjari.mardikar28@gmail.com>
2023-02-16 10:33:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const content = <div className = "content">
+    <h1>The Evolution of <br/> Airbnb's Frontend</h1>
+    <p>@spikebrehm</p>
+   <img src="https://cdn5.f-cdn.com/contestentries/995221/14777422/58eb66f38d645_thumb900.jpg" width="120px"/>
+</div>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(content)
